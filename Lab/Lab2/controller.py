31e16ca4f063a64c6eba77d5444473a1d297039e
HelloIamEric <ericdongcannn@hotmail.com>
2021-01-19 11:42:52 -0800

    created docstrings for controller

--- Lab/Lab2/controller.py
+++ Lab/Lab2/controller.py
@@ -1,10 +1,19 @@
+"""This module implements a Controller class for Asteroid class"""
+
+
 from Lab.Lab2.asteroid import Asteroid
 from datetime import datetime
 import time
 
 
 class Controller:
+    """
+    This class simulates 100 asteroids.
+    """
     def __init__(self):
+        """
+        Initialize a new Controller, and fills it with 100 randomly generated Asteroids.
+        """
         self.list_of_asteroids = []
         i = 0
         while i <= 99:
@@ -12,6 +21,12 @@ class Controller:
             i += 1
 
     def simulate(self, seconds: int):
+        """
+        Simulate asteroid movement for all Asteroids once per second for specified amount of seconds.
+
+        :param seconds: An integer greater than 0.
+        :precondition: seconds must be an int greater than 0
+        """
         print("Simulation Start Time: ", datetime.now())
         time.sleep((1000000 - datetime.now().microsecond) / 1000000)
         print("\n Moving Asteroids!\n -----------------")
