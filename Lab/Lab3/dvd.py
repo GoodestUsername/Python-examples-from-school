aa321c276922a27a4c1ade7a7c0cec668acd5c86
HelloIamEric <ericdongcannn@hotmail.com>
2021-01-26 09:17:35 -0800

    created dvd and journal classes

--- Lab/Lab3/dvd.py
+++ Lab/Lab3/dvd.py
@@ -1,26 +1,29 @@
 from Lab.Lab3.libraryitem import LibraryItem
 
 
-class Book(LibraryItem):
+class DVD(LibraryItem):
     """
-    Represents a single book in a library which is identified through
+    Represents a single journal in a library which is identified through
     it's call number.
     """
 
-    def __init__(self, call_num, title, num_copies, author):
+    def __init__(self, call_num, title, num_copies, release_date, region_code):
         """
         :param call_num: a string
         :param title: a string
         :param num_copies: an int
-        :param author: a string
+        :param release_date: a string
+        :param region_code: a string
         :precondition call_num: a unique identifier
         :precondition num_copies: a positive integer
         """
-        self._author = author
+        self._release_date = release_date
+        self._region_code = region_code
         super().__init__(call_num, title, num_copies)
 
     def __str__(self):
         return f"---- Book: {self.get_title()} ----\n" \
                f"Call Number: {self.call_number}\n" \
                f"Number of Copies: {self._num_copies}\n" \
-               f"Author: {self._author}"
+               f"Release date: {self._release_date}\n" \
+               f"Region code: {self._region_code}"
--- Lab/Lab3/journal.py
+++ Lab/Lab3/journal.py
@@ -1,26 +1,29 @@
 from Lab.Lab3.libraryitem import LibraryItem
 
 
-class Book(LibraryItem):
+class Journal(LibraryItem):
     """
-    Represents a single book in a library which is identified through
+    Represents a single journal in a library which is identified through
     it's call number.
     """
 
-    def __init__(self, call_num, title, num_copies, author):
+    def __init__(self, call_num, title, num_copies, issue_number, publisher):
         """
         :param call_num: a string
         :param title: a string
         :param num_copies: an int
-        :param author: a string
+        :param issue_number: a string
+        :param publisher: a string
         :precondition call_num: a unique identifier
         :precondition num_copies: a positive integer
         """
-        self._author = author
+        self._issue_number = issue_number
+        self._publisher = publisher
         super().__init__(call_num, title, num_copies)
 
     def __str__(self):
         return f"---- Book: {self.get_title()} ----\n" \
                f"Call Number: {self.call_number}\n" \
                f"Number of Copies: {self._num_copies}\n" \
-               f"Author: {self._author}"
+               f"Issue number: {self._issue_number}\n" \
+               f"Publisher: {self._publisher}"
