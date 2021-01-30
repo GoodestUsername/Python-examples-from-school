29b802181f9a708573ce848c396ce9ba5ff4259d
HelloIamEric <ericdongcannn@hotmail.com>
2021-01-30 15:54:47 -0800

    added names attribute to journal class

--- Lab/Lab3/journal.py
+++ Lab/Lab3/journal.py
@@ -7,11 +7,12 @@ class Journal(LibraryItem):
     it's call number.
     """
 
-    def __init__(self, call_num, title, num_copies, issue_number, publisher):
+    def __init__(self, call_num, title, num_copies, names, issue_number, publisher):
         """
         :param call_num: a string
         :param title: a string
         :param num_copies: an int
+        :param names: a string
         :param issue_number: a string
         :param publisher: a string
         :precondition call_num: a unique identifier
@@ -19,11 +20,13 @@ class Journal(LibraryItem):
         """
         self._issue_number = issue_number
         self._publisher = publisher
+        self._names = names
         super().__init__(call_num, title, num_copies)
 
     def __str__(self):
         return f"---- Book: {self.get_title()} ----\n" \
                f"Call Number: {self.call_number}\n" \
                f"Number of Copies: {self._num_copies}\n" \
+               f"Names: {self._names}\n" \
                f"Issue number: {self._issue_number}\n" \
                f"Publisher: {self._publisher}"
