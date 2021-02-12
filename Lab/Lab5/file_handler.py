dcfe406d8d885223b60ca32316cc152d836b6bde
HelloIamEric <ericdongcannn@hotmail.com>
2021-02-12 14:34:25 -0800

    updated file handler class

--- Lab/Lab5/file_handler.py
+++ Lab/Lab5/file_handler.py
@@ -1,21 +1,42 @@
 from enum import Enum
+from pathlib import Path
+import json
 
 
 class FileExtensions(Enum):
     TXT = ".txt"
     JSON = ".json"
 
+    @classmethod
+    def is_valid_extension(cls, file_extension):
+        return file_extension in cls._value2member_map_
+
 
 class FileHandler:
     @staticmethod
     def load_data(path, file_extension):
-        pass
+        if not Path(path).exists():
+            raise FileNotFoundError
+
+        if FileExtensions.is_valid_extension(file_extension):
+            with open(path, mode='r', encoding='utf-8') as data_file:
+                if file_extension is FileExtensions.JSON.value:
+                    return json.load(data_file)
+                else:
+                    return data_file.read()
+
+        else:
+            raise InvalidFileTypeError("File type is not supported exception")
 
     @staticmethod
     def write_lines(path, lines):
-        pass
+        if not Path(path).exists():
+            raise FileNotFoundError
+
+        with open(path, mode='a', encoding='utf-8') as data_file:
+            data_file.write(lines)
 
 
-class InvalidFileTypeClass(Exception):
+class InvalidFileTypeError(Exception):
     def __init__(self, my_msg):
         super().__init__(my_msg)
